c513bfaeabfa8d6480b25c9b884f0ed11fa845ea
wangke10 <wangke10@lenovo.com>
2020-10-13 17:34:39 +0800

    update

---
+++ WindowsProject1/WindowsProject1/TemplateTest.hpp
@@ -0,0 +1,211 @@
+﻿#pragma once
+#include <iostream>
+#include <tuple>  
+#include <time.h>
+
+#pragma once
+#define HAS_MEMBER(member)\
+template<typename T, typename... Args>struct has_member_##member\
+{\
+private:\
+		template<typename U> static auto Check(int) -> decltype(std::declval<U>().member(std::declval<Args>()...), std::true_type()); \
+	template<typename U> static std::false_type Check(...);\
+public:\
+	enum{value = std::is_same<decltype(Check<T>(0)), std::true_type>::value};\
+};\
+
+HAS_MEMBER(Foo)
+HAS_MEMBER(Before)
+HAS_MEMBER(After)
+
+//template<class T, typename... Args>
+//using has_before_t = decltype(std::declval<T>().before(std::declval<Args>()...));
+
+template<typename Func, typename... Args>
+struct Aspect 
+{
+	Aspect(Func&& f) : m_func(std::forward<Func>(f))
+	{
+	}
+
+	template<typename T>
+	typename std::enable_if<has_member_Before<T, Args...>::value&&has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		aspect.Before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
+		m_func(std::forward<Args>(args)...);//核心逻辑
+		aspect.After(std::forward<Args>(args)...);//核心逻辑之后的切面逻辑
+	}
+
+	template<typename T>
+	typename std::enable_if<has_member_Before<T, Args...>::value && !has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		aspect.Before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
+		m_func(std::forward<Args>(args)...);//核心逻辑
+	}
+
+	template<typename T>
+	typename std::enable_if<!has_member_Before<T, Args...>::value&&has_member_After<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	{
+		m_func(std::forward<Args>(args)...);//核心逻辑
+		aspect.After(std::forward<Args>(args)...);//核心逻辑之后的切面逻辑
+	}
+
+	template<typename Head, typename... Tail>
+	void Invoke(Args&&... args, Head&&headAspect, Tail&&... tailAspect)
+	{
+		headAspect.Before(std::forward<Args>(args)...);
+		Invoke(std::forward<Args>(args)..., std::forward<Tail>(tailAspect)...);
+		headAspect.After(std::forward<Args>(args)...);
+	}
+
+private:
+	Func m_func; //被织入的函数
+};
+template<typename T> using identity_t = T;
+
+//AOP的辅助函数，简化调用
+template<typename... AP, typename... Args, typename Func>
+void Invoke(Func&&f, Args&&... args)
+{
+	Aspect<Func, Args...> asp(std::forward<Func>(f));
+	asp.Invoke(std::forward<Args>(args)..., identity_t<AP>()...);
+}
+
+struct TimeElapsedAspect
+{
+	void Before(int i)
+	{
+	}
+
+	void After(int i)
+	{
+		std::cout << "time elapsed: " << std::endl;
+	}
+
+private:
+	double m_lastTime;
+
+};
+
+struct LoggingAspect
+{
+	void Before(int i)
+	{
+		std::cout << "entering" << std::endl;
+	}
+
+	void After(int i)
+	{
+		std::cout << "leaving" << std::endl;
+	}
+};
+
+struct testhasmem
+{
+	std::wstring before()
+	{
+		std::cout << "entering" << std::endl;
+		return L"aaa";
+	}
+
+	void after(int i)
+	{
+		std::cout << "leaving" << std::endl;
+	}
+};
+
+struct testhasmem2
+{
+	std::wstring before2()
+	{
+		std::cout << "entering" << std::endl;
+		return L"aaa";
+	}
+
+	void after(int i)
+	{
+		std::cout << "leaving" << std::endl;
+	}
+};
+
+void foo(int a)
+{
+	std::cout << "real HT function: " << a << std::endl;
+}
+
+
+template< typename T>
+struct filter_helper
+{
+	static constexpr auto func()
+	{
+		return std::tuple<>();
+	}
+
+	template< class... Args >
+	static constexpr auto func(T&&, Args&&...args)
+	{
+		return filter_helper::func(std::forward<Args>(args)...);
+	}
+
+	template< class X, class... Args >
+	static constexpr auto func(X&&x, Args&&...args)
+	{
+		return std::tuple_cat(std::make_tuple(std::forward<X>(x)), filter_helper::func(std::forward<Args>(args)...));
+	}
+};
+
+template<typename T, typename... Args>
+inline auto filter(Args&&... args) {
+	return filter_helper<T>::func(std::forward<Args>(args)...);
+}
+
+class request {
+public:
+};
+
+class response {
+public:
+};
+
+	template<class AlwaysVoid,
+		template<class...> class Op, class... Args>
+	struct detector {
+		using value_t = std::false_type;
+	};
+
+	template< template<class...> class Op, class... Args>
+	struct detector< std::void_t<Op<Args...>>, Op, Args...> {
+		using value_t = std::true_type;
+	};
+
+	template<template<class...> class Op, class... Args>
+	using is_detected = typename detector<void, Op, Args...>::value_t;
+
+	template<class T, typename... Args>
+	using has_before_t = decltype(std::declval<T>().before(std::declval<Args>()...));
+
+	template<class T, typename... Args>
+	using has_after_t = decltype(std::declval<T>().after(std::declval<Args>()...));
+
+
+template<typename T, typename... Args>
+using has_before = is_detected<has_before_t, T, Args...>;
+
+template<typename T, typename... Args>
+using has_after = is_detected<has_after_t, T, Args...>;
+
+//template <typename... Args, typename F, std::size_t... Idx>
+//constexpr void for_each_l(std::tuple<Args...>& t, F&& f, std::index_sequence<Idx...>) {
+//	(std::forward<F>(f)(std::get<Idx>(t)), ...);
+//}
+
+//class AOP
+//{
+//public:
+//	template<typename Function, typename... AP>
+//	typename std::enable_if<has_member_Before<T, Args...>::value&&has_member_After<T, Args...>::value>::type invoke(request& req, response& res, Function&& f, AP&&... ap)
+//	{
+//		std::tuple<AP...> tp(std::move(ap)...);
+//	}
+//};
--- WindowsProject1/WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -3,6 +3,15 @@
 
 #include "framework.h"
 #include "WindowsProject1.h"
+#include "TemplateTest.hpp"
+#include <math.h>
+#include "XZip.h"
+
+#include <unordered_map>
+#include <deque>
+#include <string>
+#include <map>
+#include <functional>
 
 #define MAX_LOADSTRING 100
 
@@ -17,11 +26,265 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+#if _MSC_VER
+#define PlaceHolder std::_Ph
+#else
+#define PlaceHolder std::_Placeholder
+#endif
+
+template<int N, int...I>
+struct MakeSeqs : MakeSeqs<N - 1, N - 1, I...> {};
+
+template<int...I>
+struct MakeSeqs<1, I...>
+{
+	template<class T, class _Fx>
+	static auto bind(T&& obj, _Fx&& _Func)
+	{
+		return std::bind(std::forward<_Fx>(_Func), std::forward<T>(obj), PlaceHolder<I>{}...);
+	}
+};
+
+template <class T, class R, typename...Args>
+auto Bind(T* t, R(T::*f)(Args...))
+{
+	return MakeSeqs<sizeof...(Args) + 1>::bind(t, f);
+}
+
+template <class T, class R, typename...Args>
+auto Bind(R(T::*f)(Args...), T* t)
+{
+	return MakeSeqs<sizeof...(Args) + 1>::bind(t, f);
+}
+
+
+void Test(request& req, response& res)
+{
+	int i;
+	i = 0;
+}
+
+class InvokeTest
+{
+public:
+	template<typename Function>
+	void invoke(int & i, Function f)
+	{
+		f();
+	}
+
+
+	template<typename Function>
+	void InvokeFun(Function f)
+	{
+		//map_invokers_[L"1"] = std::bind(&InvokeTest::invoke,this,std::placeholders::_1, f);
+	}
+
+	void Do()
+	{
+		//InvokeFun(&InvokeTest::Test);
+		int i = 4;
+		//map_invokers_[L"1"](i);
+	}
+
+	template<typename Function, typename... AP>
+	void invoke(request& req, response& res, Function f, AP... ap)
+	{
+
+	}
+
+	template<typename Function, typename... AP>
+	void register_nonmember_func(std::wstring raw_name, Function f, const AP&... ap) {
+			this->map_invokers_[raw_name] = { raw_name, std::bind(&InvokeTest::invoke<Function, AP...>, this,
+				std::placeholders::_1, std::placeholders::_2, std::move(f), ap...) };
+	}
+
+	template<typename Function, typename... Ap>
+	void register_handler(std::wstring name, Function&& f, const Ap&... ap) {
+			register_nonmember_func(name, std::forward<Function>(f), ap...);
+	}
+
+	template<typename Function, typename... AP>
+	void set_http_handler(std::wstring name, Function&& f, AP&&... ap)
+	{
+		register_handler(name, std::forward<Function>(f), std::forward<AP>(ap)...);
+	}
+
+	//std::map<std::wstring, std::function<void(int &i)>> map_invokers_;
+
+	typedef std::pair<std::wstring, std::function<void(request&, response&)>> invoker_function;
+	std::map<std::wstring, invoker_function> map_invokers_;
+};
+
+template<typename Fn>
+void Test(int &i, Fn f)
+{
+	f();
+}
+
+template<typename Fn, class... Args>
+void Testttt(int i, Fn f, Args&&... args)
+{
+	f(std::forward<Args>(args)...);
+}
+
+template<typename Fn, class... Args>
+bool Testttt2( Fn f, Args&&... args)
+{
+	f(std::forward<Args>(args)...);
+	return true;
+}
+
+
+void Test2()
+{
+	int i;
+	i = 0;
+}
+
+void Test3(int n)
+{
+	int i;
+	i = 0;
+}
+std::map<std::wstring, std::function<void(int)>> gmap_invokers_;
+
+template<typename Fn, class... Args>
+void Add(std::wstring name, Fn fn, Args&&... args)
+{
+	gmap_invokers_[name] = std::bind(&Testttt<Fn, Args...>, std::placeholders::_1, fn, std::forward<Args>(args)...);
+}
+
+template <typename R, typename... Args>
+class FuncContainer
+{
+public:
+
+
+	template<typename Fn, typename... SubArgs>
+	void Add(std::wstring name, Fn fn, SubArgs&&... args)
+	{
+		
+		[name] = std::bind(&Testttt2<Fn, SubArgs...>, fn, std::forward<SubArgs>(args)...);
+	}
+private:
+	std::map<std::wstring, std::function<R(Args...)>> map_invokers;
+};
+
+class CTest
+{
+public:
+	void foo1(int a, std::wstring& b, float c, double d, void* p) {};
+	void foo2(int a, std::string& b, float c, double d, void* p, short f) {};
+};
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
+	CTest t;
+	auto f1 = Bind(&CTest::foo1, &t);
+	auto f2 = Bind(&CTest::foo2, &t);
+
+	int bbb = 13;
+	FuncContainer<bool> container;
+	//container.Add(L"1", Test2);
+	container.Add(L"2", Test3, bbb);
+	//InvokeTest t;
+	//t.set_http_handler(L"a", [](request& req, response& res) {});
+	//t.Do();
+	
+	Testttt<std::function<void(int)>, int>(bbb, Test3, 3);
+
+
+	//Add(L"1", Test2);
+	Add(L"2", Test3, bbb);
+	//map_invokers_[L"2"] = std::bind(&Testttt<std::function<void(int)>, int>, std::placeholders::_1, Test3, 2);
+	//int temp = 3;
+	//map_invokers_[L"1"](temp);
+	//map_invokers_[L"2"](temp);
+
+	TCHAR zipname[] = L"E:\\Inno Setup 5";
+	TCHAR zipfile[FILENAME_MAX];
+	wsprintf(zipfile, L"%s.zip", zipname);
+
+	HZIP hz = CreateZip(zipfile, 0, ZIP_FILENAME);
+	std::deque<std::wstring>folderList;
+	TCHAR folderpath[MAX_PATH] = L"E:\\Inno Setup 5";
+	folderList.push_back(std::wstring(folderpath));
+	while (folderList.size() > 0)
+	{
+		WIN32_FIND_DATA finddata;
+		std::wstring pathfile = folderList.front();
+		std::wstring subdir = pathfile;
+		pathfile.append(L"\\*.*");
+		HANDLE handleFind = FindFirstFile(pathfile.c_str(), &finddata); //FindFirstFile
+		DWORD err = GetLastError();
+		folderList.pop_front();
+		if (handleFind != INVALID_HANDLE_VALUE)
+		{
+			do
+			{
+				if (finddata.cFileName[0] == '.')
+					continue;
+				else if (finddata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				{
+					std::wstring subpath = subdir;
+					subpath.append(L"\\");
+					subpath.append(finddata.cFileName);
+					folderList.push_back(subpath);
+				}
+				else
+				{
+					std::wstring filename = subdir;
+					filename.append(L"\\");
+					filename.append(finddata.cFileName);
+					std::wstring zipdir = filename.substr(wcslen(folderpath) + 1);
+					ZipAdd(hz, zipdir.c_str(), (void*)filename.c_str(), 0, ZIP_FILENAME);
+				}
+			} while (FindNextFile(handleFind, &finddata));
+			FindClose(handleFind);
+		}
+	}
+	CloseZip(hz);
+	
+	has_before_t<testhasmem> a;
+	a = L"adsf";
+	testhasmem t1;
+	testhasmem2 t2;
+
+	//auto aaa = is_detected< decltype(t1)>::value;
+	bool has_befor_mtd = has_before<decltype(t1)>::value;
+	has_befor_mtd = has_before<decltype(t2)>::value;
+
+   	std::unordered_multimap<std::wstring, int> test;
+	test.insert({ L"abc", 1 });
+	test.insert({ L"bbc", 2 });
+	test.insert({ L"dbc", 3 });
+	test.insert({ L"abc", 4 });
+	//test[L"abc"] = 1;
+	//test[L"bbc"] = 2;
+	//test[L"dbc"] = 3;
+	//test[L"abc"] = 4;
+
+ 	auto samePair = test.equal_range(L"abc");
+	for (auto i = samePair.first; i != samePair.second; i++)
+	{
+		i->first;
+	}
+
+	Invoke<LoggingAspect, TimeElapsedAspect>(&foo, 1); //织入方法
+
+	Invoke<TimeElapsedAspect, LoggingAspect>(&foo, 1);
+
+	//HandEvent(Do, &person::foo1, log_t{});
+	/*auto tp = filter(&person::foo1, log_t{});
+	auto lm = [](auto... ap) {
+		return;
+	};
+	std::apply(lm, std::move(tp));*/
+
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
